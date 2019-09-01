6fb9ee157bd8b1b104c70f7e150dd6774ac43d55
Oliver Lee <oliverzlee@gmail.com>
2019-09-01 17:25:45 +0200

    Print timing phrase with time delays
    Use tokio to print out the timing phrase for a morse code phrase with
    time delays. Add functions 'into_timing()' that consume input in order
    to resolve lifetime problems when using the iterator asynchronously.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use morse::code::Code;
 use std::str::FromStr;
 
+use tokio::prelude::*;
+use tokio::timer::Delay;
+
+use std::time::{Duration, Instant};
+
 fn main() {
     let s = "Hello, world!";
     println!("{}", s);
@@ -15,4 +20,19 @@ fn main() {
             .collect::<Vec<String>>()
             .join("")
     );
+
+    let task = stream::iter_ok(p.into_timing())
+        .for_each(|x| {
+            Delay::new(Instant::now() + Duration::from_millis(100)).and_then(move |_| {
+                print!("{}", x.to_string());
+                std::io::stdout().flush().unwrap();
+
+                future::ok(())
+            })
+        })
+        .map_err(|_| ());
+
+    println!("starting tokio transmit");
+    tokio::run(task);
+    println!();
 }

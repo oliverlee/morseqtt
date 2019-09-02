56f6af61ae5b54b31492c08130b78f36d3cf5614
Oliver Lee <oliverzlee@gmail.com>
2019-09-02 23:39:14 +0200

    Parse command line arguments
    Add arguments to set mqtt broker host and port, publisher topic, on/off
    payloads, and morse code dot duration. Topic and payload arguments are
    not yet used.
    
    This commit adds getopts as a dependency.

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,115 @@ use std::sync::{Arc, Mutex};
 use std::time::Duration;
 use tokio::prelude::*;
 
-const DOT_DURATION: Duration = Duration::from_millis(50);
 const CLIENT_NAME: &str = "morseqtt";
 
-fn main() {
-    let host = "localhost";
-    let port = 1883;
+// concat!() doesn't accept const variables so we define a macro so the values aren't written twice.
+macro_rules! default_host {
+    () => {
+        "localhost"
+    };
+}
+macro_rules! default_port {
+    () => {
+        1883
+    };
+}
+macro_rules! default_duration_ms {
+    () => {
+        50
+    };
+}
 
-    let mqtt_options = MqttOptions::new(CLIENT_NAME, host, port);
+fn program_opts() -> getopts::Options {
+    let mut opts = getopts::Options::new();
+
+    opts.optopt(
+        "h",
+        "host",
+        concat!("mqtt host to connect to. [", default_host!(), "]"),
+        "<host>",
+    );
+    opts.optopt(
+        "p",
+        "port",
+        concat!("network port to connect to. [", default_port!(), "]"),
+        "<port>",
+    );
+    opts.optopt(
+        "d",
+        "duration",
+        concat!(
+            "morse code dot duration, in milliseconds. [",
+            default_duration_ms!(),
+            "]"
+        ),
+        "<milliseconds>",
+    );
+    opts.optflag("", "help", "print this help menu");
+
+    opts
+}
+
+fn print_usage(program: &str, opts: getopts::Options) {
+    let brief = format!(
+        concat!(
+            "Usage: {} [options] <topic> <on_payload> <off_payload>\n\n",
+            "Translate input to morse code and send using MQTT."
+        ),
+        program
+    );
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = program_opts();
+    let mut matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => {
+            println!("{}", f.to_string());
+            println!();
+            print_usage(&program, opts);
+            return;
+        }
+    };
+    if matches.opt_present("help") {
+        print_usage(&program, opts);
+        return;
+    }
+    if matches.free.len() != 3 {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let host = matches
+        .opt_str("host")
+        .unwrap_or(default_host!().to_string());
+    let port = matches
+        .opt_str("port")
+        .map(|s| s.parse::<u16>().unwrap())
+        .unwrap_or(default_port!());
+    let duration = Duration::from_millis(
+        matches
+            .opt_str("duration")
+            .map(|s| s.parse::<u64>().unwrap())
+            .unwrap_or(default_duration_ms!()),
+    );
+    let off_payload = matches.free[0].as_str();
+    let on_payload = matches.free[1].as_str();
+    let topic = matches.free[2].as_str();
+    println!("topic: {}", topic);
+    println!("on payload: {}", on_payload);
+    println!("off payload: {}", off_payload);
+    println!("duration: {:?}", duration);
+    println!("host: {}", host);
+    println!("port: {}", port);
+
+    let mqtt_options = MqttOptions::new(CLIENT_NAME, &host, port);
     let (mut client1, _) = MqttClient::start(mqtt_options).unwrap();
     let mut client2 = client1.clone();
-
     println!("Connected to {}:{} as {}", host, port, CLIENT_NAME);
 
     // Create a Key for transmission.
@@ -56,7 +154,7 @@ fn main() {
             transmit_with_dur(
                 Arc::clone(&k),
                 Code::from_str(s).unwrap().into_timing(),
-                DOT_DURATION,
+                duration,
             )
             .and_then(|_| {
                 println!("Transmission complete.");
